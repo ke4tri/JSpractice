e3d218bfe9d6c62d4d16940e0b3e0409bb09cf15
Wayne Collier <ke4tri@gmail.com>
2018-09-04 08:17:09 -0500

    more string practice

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+};
 
 const sonnet = "How like a winter hath my absence been From thee, the pleasure of the fleeting year! What freezings have I felt, what dark days seen! What old December's bareness everywhere! And yet this time removed was summer's time; The teeming autumn, big with rich increase, Bearing the wanton burden of the prime, Like widow'd wombs after their lords' decease: Yet this abundant issue seemed to me But hope of orphans, and unfathered fruit; For summer and his pleasures wait on thee, And, thou away, the very birds are mute: Or, if they sing, 'tis with so dull a cheer, That leaves look pale, dreading the winter's near.";
 
@@ -12,7 +16,14 @@ const sonnet = "How like a winter hath my absence been From thee, the pleasure o
 // sonnetDiv.innerHTML = `<h3>${newSonnet}</h3>`;
 
 
-
+let str = "I am cow cow Please locate where too 'locate' occurs!!"
+let pos = str.lastIndexOf("occurs");
+console.log('Print pos: ' + pos);
+console.log('Length : ' + str.length);
+console.log('Locate : ' + str.indexOf("locate",27));
+console.log('Search too: ' + str.search('too'));
+console.log('Search am: ' + str.search('am'));
+let str3 = ('Search am: ' + str.search('am'));
 
 ///////////////////////////////////////
 
