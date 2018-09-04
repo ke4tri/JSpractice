c2403505fd2fb0736003b5031b2f46a7c1620399
Wayne Collier <ke4tri@gmail.com>
2018-09-03 21:27:42 -0500

    practice

---
+++ main.js
@@ -0,0 +1,50 @@
+
+
+var a = "time";
+var b = "time";
+var c = time;
+
+
+///////////////////////////////////////
+
+
+// const SPENDING_THRESHOLD = 200;
+// const TAX_RATE = 0.08;
+// const PHONE_PRICE = 99.99;
+// const ACCESSORY_PRICE = 9.99;
+
+// var bank_balance = 303.91;
+// var amount = 0;
+
+
+// while (amount < bank_balance){
+//     amount = amount + PHONE_PRICE;
+// };
+
+
+
+//////////////////////////////////////
+
+// const price = 200;
+// let phones = 0; 
+// let money = 3000;
+
+
+
+// while(phones < 15){ //The 15 is created in one 'transaction'
+//     phones = money / price;// And not a loop (one loop);
+//     money = money - (price*phones);
+//     cost = price*phones;
+// };
+
+// function tax(amount){
+//     return (amount * .0925) + amount;
+// };
+
+// let cost_tax = tax(cost);
+// console.log("Price of phone: " + "$" + price);
+// console.log("Number of phones bought: " + phones);
+// console.log("Money left after purchase: " + "$"  + money);
+// console.log("Total cost of the phones: " + "$"  + cost);
+// console.log("Total with tax: " + "$"  + cost_tax);
+
